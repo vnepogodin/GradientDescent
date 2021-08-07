83805574f8d721a8a72c0f13969955d65e879e88
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2021-08-07 22:57:08 +0400

    Refactor

--- header.hpp
+++ header.hpp
@@ -1,22 +1,29 @@
-#pragma once
+#ifndef HEADER_HPP_
+#define HEADER_HPP_
+
 enum { widthI = 400, heightI = 400 };
 
 // Includes
+#include "nn.hpp"
+#include <deque>
 #include <iostream>
 #include <thread>
-#include <deque>
-#include <nn.hpp>
 
 // SFML
 #include <SFML/Graphics.hpp>
 
-// Namespaces
-using namespace std;
-using namespace sf;
-
 // Constants
-static const float heightF = 400.f, widthF = 400.f;
+static constexpr float heightF = 400.f, widthF = 400.f;
+static constexpr double learning_rate = 0.01;
+static constexpr std::uint8_t red = 235, green = 92, blue = 83;
 
 // Variables
-ContextSettings settings(32, 0, 8);
-RenderWindow window(VideoMode(widthI, heightI), "Neural Networks", Style::Default, settings);
+float m = 0, b = 0;
+NeuralNetwork nn;
+
+sf::ContextSettings settings(32, 0, 8);
+sf::RenderWindow window(sf::VideoMode(widthI, heightI), "Neural Networks", sf::Style::Default, settings);
+
+static std::deque<sf::CircleShape> shapes;
+
+#endif  // HEADER_HPP_
--- main.cpp
+++ main.cpp
@@ -1,87 +1,73 @@
-﻿#include <header.hpp>
+/* g++ -O2 -flto main.cpp -lsfml-system -lsfml-window -lsfml-graphics -pthread */
+#include "header.hpp"
 
-NeuralNetwork *ptr;
-
-deque<CircleShape> shape;
-
-int red = 235, green = 348, blue = 339;
-float m = 0, b = 0;
-
-float mnp(float n, int start1, int stop1, int start2, int stop2) {
+static inline float mnp(float n, int start1, int stop1, int start2, int stop2) {
     return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
 }
 
 void setup() {
-    NeuralNetwork brain = *new NeuralNetwork(3, 3, 2);
-    ptr = &brain;
+    nn = NeuralNetwork(2, 4, 1);
+    nn.setLearningRate(learning_rate);
 }
 
 void gradientDescent() {
-    float learning_rate = 0.01;
+    for (const auto& shape : shapes) {
+        const float& x = shape.getPosition().x;
+        const float& y = shape.getPosition().y;
 
-    int i = 0;
-    while(i < shape.size()) {
-        float x = shape[i].getPosition().x,
-              y = shape[i].getPosition().y,
-
-              guess = m * x + b,
-
-              error = y - guess;
+        const float& guess = m * x + b;
+        const float& error = y - guess;
 
         m += error / x * learning_rate;
         b += error * learning_rate;
-
-        i++;
     }
-
 }
 
 void drawLine() {
     float x1 = 0, y1 = m * x1 + b,
-        x2 = 0, y2 = m * x2 + b;
+          x2 = 0, y2 = m * x2 + b;
 
     x1 = mnp(x1, 0, 1, 0, widthI);
     y1 = mnp(y1, 0, 1, 0, heightI);
     x2 = mnp(x2, 0, 1, widthI, 0);
     y2 = mnp(y2, 0, 1, heightI, 0);
 
-    Vertex vertices[2] = {
-            Vertex(Vector2f(x1, y1), Color::White),
-            Vertex(Vector2f(x2, y2), Color::White),
+    const sf::Vertex vertices[2] = {
+        sf::Vertex(sf::Vector2f(x1, y1), sf::Color::White),
+        sf::Vertex(sf::Vector2f(x2, y2), sf::Color::White),
     };
 
-    window.draw(vertices, 2, Lines);
+    window.draw(vertices, 2, sf::Lines);
 }
 
 void mousePressed() {
-    Vector2f mousepos = window.mapPixelToCoords(Mouse::getPosition(window));
-    float x = mousepos.x, y = mousepos.y;
+    const auto& mousepos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+    const double x       = mousepos.x;
+    const double y       = mousepos.y;
 
-    CircleShape point;
+    sf::CircleShape point;
     point.setRadius(8);
-    point.setOutlineColor(Color(255));
+    point.setOutlineColor(sf::Color::White);
     point.setPosition(x, y);
 
-    shape.push_back(point);
+    shapes.push_back(point);
 }
 
 void itLegal() {
-    if (shape.size() > 1) {
+    if (shapes.size() > 1) {
         gradientDescent();
         drawLine();
     }
 }
 
 void draw() {
-    window.clear(Color(red, green, blue));
+    window.clear({red, green, blue});
 
-    int i = 0;
-    while(i < shape.size()) {
-        window.draw(shape[i]);
-        i++;
+    for (const auto& shape : shapes) {
+        window.draw(shape);
     }
 
-    thread statements(itLegal);
+    std::thread statements(itLegal);
 
     statements.join();
 
@@ -90,21 +76,21 @@ void draw() {
 
 int main() {
 
-    thread pre(setup);
+    std::thread pre(setup);
     pre.join();
 
     while (window.isOpen()) {
-        thread first(setup);
+        std::thread first(setup);
         first.join();
-        Event event;
+        sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == Event::Closed)
+            if (event.type == sf::Event::Closed)
                 window.close();
-            else if (event.type == Event::MouseButtonPressed)
+            else if (event.type == sf::Event::MouseButtonPressed)
                 mousePressed();
         }
 
-        thread second(draw);
+        std::thread second(draw);
         second.join();
     }
 
